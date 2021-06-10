7e95ac85e7d684be3ec446bb2306805372033089
issuaa <81679353+issuaa@users.noreply.github.com>
2021-06-10 06:34:12 +0200

    Create Math.sol

---
+++ contracts/openzeppelin/Math.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Standard math utilities missing in the Solidity language.
+ */
+library Math {
+    /**
+     * @dev Returns the largest of two numbers.
+     */
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    /**
+     * @dev Returns the average of two numbers. The result is rounded towards
+     * zero.
+     */
+    function average(uint256 a, uint256 b) internal pure returns (uint256) {
+        // (a + b) / 2 can overflow, so we distribute
+        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
+    }
+
+    
+    /*
+    // babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+    function sqrt(uint y) internal pure returns (uint256) {
+        uint256 z;
+        if (y > 3) {
+            uint256 z = y;
+            uint256 x = (y / 2) + 1;
+            while (x < z) {
+                z = x;
+                x = ((y / x) + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+        return(z);
+    }
+    */
+    
+    /**
+    * Calculate sqrt (x) rounding down, where x is unsigned 256-bit integer
+    * number.
+    *
+    * @param x unsigned 256-bit integer number
+    * @return unsigned 128-bit integer number
+    */
+    function sqrtu (uint256 x) internal pure returns (uint128) {
+        unchecked {
+            if (x == 0) return 0;
+            else {
+                uint256 xx = x;
+                uint256 r = 1;
+                if (xx >= 0x100000000000000000000000000000000) { xx >>= 128; r <<= 64; }
+                if (xx >= 0x10000000000000000) { xx >>= 64; r <<= 32; }
+                if (xx >= 0x100000000) { xx >>= 32; r <<= 16; }
+                if (xx >= 0x10000) { xx >>= 16; r <<= 8; }
+                if (xx >= 0x100) { xx >>= 8; r <<= 4; }
+                if (xx >= 0x10) { xx >>= 4; r <<= 2; }
+                if (xx >= 0x8) { r <<= 1; }
+                r = (r + x / r) >> 1;
+                r = (r + x / r) >> 1;
+                r = (r + x / r) >> 1;
+                r = (r + x / r) >> 1;
+                r = (r + x / r) >> 1;
+                r = (r + x / r) >> 1;
+                r = (r + x / r) >> 1; // Seven iterations should be enough
+                uint256 r1 = x / r;
+                return uint128 (r < r1 ? r : r1);
+            }
+        }
+    }
+}
